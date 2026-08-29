c2b63c13625da8ad8368ad2e3b5fce009120eb9e
Egor <611221201@gms.ndhu.edu.tw>
2026-08-29 14:23:15 +0800

    first

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+from .llava import LlavaModel
+from .qwen_vl import QwenVLModel
+
+MODEL_REGISTRY = {
+    "llava": LLaVAModel,
+    "qwen_vl": QwenVLModel,
+}
+
+def build_model(model_name: str, **kwargs):
+    model_cls = MODEL_REGISTRY.get(model_name.lower())
+    if model_cls is None:
+        raise ValueError(f"Unsupported model: {model_name}. Supported models: {list(MODEL_REGISTRY.keys())}")
+    return model_cls(**kwargs)
